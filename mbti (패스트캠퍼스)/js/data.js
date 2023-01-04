c5bada14e9aa0031426a609fc9b21a846ee87648
sihyeon1 <121509856+sihyeon1@users.noreply.github.com>
2023-01-04 20:47:17 +0900

    Add files via upload

---
+++ mbti (패스트캠퍼스)/js/data.js
@@ -0,0 +1,310 @@
+export const questions = [
+  {
+    number: '01',
+    question: '친구들이 밥먹자고 부른다!<br>근데 나보고 사랜다.',
+    choices: [
+      { text: '밥먹고 뭐하고 놀지?? 재밌겠다 ㅎㅎ<br>(호다닥 옷입고 나간다)', value: 'e' },
+      { text: '미친놈들인가? 니들 밥을 왜 내가 사;;<br>(절대 혼자가 좋아서가 아닌, 밥사기 싫어서 안나간다)', value: 'i' }
+    ]
+  },
+  {
+    number: '02',
+    question: '할 일은 많지만 미뤄두고 영화를 본다!<br>근데 영화가 재미가 없다.',
+    choices: [
+      { text: '이게 영화냐?<br>연기도 못하고 연출도 못하고. 시간만 버렸네', value: 's' },
+      { text: '히히 일하는거 빼고는 다 재밌어 >< !!', value: 'n' }
+    ]
+  },
+  {
+    number: '03',
+    question: '친구랑 걷다가 엄청<br>잘생긴/예쁜 사람을 봤다!!',
+    choices: [
+      { text: '와우 졸라 결혼하고싶어 날 가져요 ㅠㅠ', value: 'f' },
+      { text: '잘생겼네 / 예쁘네', value: 't' }
+    ]
+  },
+  {
+    number: '04',
+    question: '이따 친구랑 만나기로 했는데 <br>친구가 까먹었을것 같다!',
+    choices: [
+      { text: '야 이따 약속 알지?  자는거 아니지??<br>10분마다 문자를 보낸다', value: 'j' },
+      { text: '아몰랑 기억 하겠지~<br> 이따 안나오면 연락 해봐야겠다 ', value: 'p' }
+    ]
+  },
+  {
+    number: '05',
+    question: '친구가 소개해준 애가<br> 마스크 꼈을땐 너무 좋았는데<br>마스크를 벗으니 너무 못생겼다!',
+    choices: [
+      { text: '오와 너 마스크 쓴거랑 안쓴거 차이가 크네. ..하하', value: '' },
+      { text: '헐 ㅁㅊ 너 왜 그렇게 생겼냐.. 사람이 어떻게...<br> 와. .. 사람 맞아?? ', value: '' }
+    ]
+  },
+  {
+    number: '06',
+    question: '친구가 <br>"오늘 날씨가 좋네~" 라고 한다! 나는?',
+    choices: [
+      { text: '어쩌라고', value: '' },
+      { text: '그런김에 한강 ㄱ?', value: '' }
+    ]
+  },
+  {
+    number: '07',
+    question: '친구랑 "호랑이랑 사자중 <br>누가 더 세냐" 로 싸웠다<br>다음날 나는?',
+    choices: [
+      { text: '내 말이 맞다는 증거 자료를<br>여기저기서 찾아온다', value: '' },
+      { text: '싸운거 까먹음', value: '' }
+    ]
+  },
+  {
+    number: '08',
+    question: '공부 할려고 책을 폈는데<br> 하나도 모르겠다!',
+    choices: [
+      { text: '그래도 책 편 김에 쪼금만 더 봐볼까', value: '' },
+      { text: '에라이 집중 안되는 김에 걍 내일 해야겠다', value: '' }
+    ]
+  },
+  {
+    number: '09',
+    question: '친구랑 로또사면서<br> "우리 1등되면 반띵 ㄱ"<br> 라고 했는데 진짜 1등이 됐다!!',
+    choices: [
+      { text: 'ㅇ? 난 그런 말 한 기억이 없는데??', value: '' },
+      { text: '야! 나 1등이다!! 너 12억7천만원 줄게!!!! <br>는 무슨 해외로 뜬다', value: '' }
+    ]
+  },
+  {
+    number: '10',
+    question: '아이폰이 가지고싶어서 여기저기 이벤트 응모를 했는데<br>애플워치가 당첨 됐다고 문자가 왔다!',
+    choices: [
+      { text: '앞으로 구르고 뒤로 구르고<br> 와 태어나서 이런거 처음 당첨돼봐요 <br>감사합니다ㅠㅠ  하고 받는다', value: '' },
+      { text: '아니 아이폰 달라고요 뭔 애플워치<br> 필요도 없는거 ;; <br>하고 무시한다', value: '' }
+    ]
+  },
+]
+export const results = [
+  {
+    title: '핑크아령 시현!!', //0번 entj 완료
+    character: '/시현 이미지/운동.jpg',
+    results: [
+      '무거운 아령이 없어서 핑크아령을 3개씩 들고 있어요!',
+      '언제나 철저하게 준비를 하고<br>활동적이에요💪',
+      '친구랑 헬스장을 가면 죽기 직전까지 굴려요~ ',
+      '지하철에서 껌파는 아줌마가 다가오면<br>매몰차게 거절해요!'
+    ],
+    jobs: ['개말라 시현<br>짱구 시현', '없음'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '앞치마소녀 시현!!', //1번 entp 완료
+    character: '/시현 이미지/앞치마.jpg',
+    results: [
+      '정신이 나간게 아니에요!',
+      '머리를 다친게 아니에요🤕',
+      '게이가 아니에요!',
+      '박학다식하고 독창적으로 끊임없이 <br>새로운 도전을 하는것 뿐이에요!'
+    ],
+    jobs: ['사랑니 뽑 시현<br>올빽 시현', '없음'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '열올라 시현!!', //2번 estp 완료
+    character: '/시현 이미지/머리아픔.jpg',
+    results: [
+      '머리가 아파서 병원을 갔는데<br>대기가 길어서 열이 더 올랐어요!',
+      '그렇지만 느긋하게 기다리며 <br>"아픈 사람이 많나보다~" 라고 생각 해요🥱',
+      '친구들이 "사자랑 호랑이중 누가 더 세냐" <br>로 싸우고 있으면 "우리 엄마가 더 세."<br>하고 싸움을 끝내줘요!',
+      '집에서 엄마한테 까불다가 몇대 맞아요!'
+    ],
+    jobs: ['블랙핑크 시현<br>시현의 손발', '개말라 시현<br>드론 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '통스팸 시현!!', //3번 esfp 완료
+    character: '/시현 이미지/통스팸.jpg',
+    results: [
+      '상체가 통스팸이에요!',
+      '호기심이 많고 개방적이에요🫣',
+      '운동한지 한달도 안 지나서 <br>몸이 좋아졌다고 착각하는 스타일이에요~',
+      '친구랑 밥먹으면 1원 단위까지 <br>더치페이 해요!'
+    ],
+    jobs: ['블랙핑크 시현<br>시현의 손발', '개말라 시현<br>드론 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '양갈래 시현!!', //4번 esfj 완료
+    character: '/시현 이미지/양갈래.jpg',
+    results: [
+      '학교에서 양갈래로 다녀요!<br>(하지만 제정신이에요!)',
+      '사람에 대한 관심이 많아서 <br>지나가는 사람들을 다 쳐다봐요🤓',
+      '사람 이름을 성 떼고 불러서 <br>다들 부담스러워해요!',
+      '구걸하는 사람을 보면 자꾸 돈을 뜯길까봐<br>일부러 사람 없는 길로만 다녀요!'
+    ],
+    jobs: ['통스팸 시현<br>열올라 시현', '사랑니 뽑 시현<br>사와디캅 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '사와다캅 시현!!', //5번 enfj 완료
+    character: '/시현 이미지/사와디캅.jpg',
+    results: [
+      '사와디캅~ <br>처럼 보이지만 사실 스쿼트 하는 중이에요!',
+      '사교적이라 사람을 만날때마다 <br>"사와디캅~"하고 인사해요🙏',
+      '타인의 의견을 존중해서 <br>내 말이 맞는 것 같아도 딱히 반박하지 않아요! ',
+      '하지만 욕을 들으면, <br>죽기 직전까지 패버리고 싶어요!'
+    ],
+    jobs: ['짱구 시현<br>아재셀카 시현', '얼굴 없어 시현<br>정신나감 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '개말라 시현!!', //6번 infp 완료
+    character: '/시현 이미지/복근.jpg',
+    results: [
+      '외복사근 생긴거 자랑해요!',
+      '성실하고 이해심이 많으며,개방적이에요😳',
+      '잘 표현하지 않으나,<br> 사실 일 안하고 돈 벌고 싶어요!',
+      '로또를 맨날사는데 당첨은 되지를 않아요!'
+    ],
+    jobs: ['사와디캅 시현<br>핑크아령 시현', '열올라 시현<br>얼굴 없어 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '아재셀카 시현!!', //7번 isfp 완료
+    character: '/시현 이미지/아재셀카.jpg',
+    results: [
+      '그냥 한번 찍어봤어요!',
+      '온화하고 삶에 여유를 만끽하는 편이에요🫠',
+      '귀찬은 일은 나중으로 미뤄요!',
+      '급한 일도 하기 싫어지면 그냥 안해요!'
+    ],
+    jobs: ['사와디캅 시현<br>양갈래 시현<br>정신나감 시현', '개말라 시현<br>드론 시현<br>사랑니 뽑 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '블랙핑크 시현!!', //8번 isfj 완료
+    character: '/시현 이미지/블랙핑크.jpg',
+    results: [
+      '블랙과 핑크 컨셉 패션이에요!',
+      '차분하고 헌신적이에요🤤',
+      '인내심이 강해, 해외 택배를 자주 시켜요!',
+      '친구가 울다가 웃으면 <br>"슬프다가 기뻐졌구나!" 라고 생각해요!'
+    ],
+    jobs: ['아재셀카 시현<br>얼굴 없어 시현', '개말라 시현<br>드론 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '사랑니 뽑 시현!!', //9번 infj 완료
+    character: '시현 이미지/사랑니.jpg',
+    results: [
+      '사랑니 뽑아서 부었어요ㅠㅠ',
+      '높은 통찰력으로, 사람들에게 영감을 줄 수도 있지만,<br>할멈을 줄 수는 없어요👨‍🦳',
+      '남아있을 이들을 위해 , 아픔을 참을 수 있어요!',
+      '치과 냄새는 왜 이렇게 싫은지 모르겠어요! '
+    ],
+    jobs: ['드론 시현<br>앞치마소녀 시현', '블랙핑크 시현<br>양갈래 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '정신나감 시현!!', //10번 estj 완료
+    character: '/시현 이미지/정신나감.jpg',
+    results: [
+      '밤새도록 편집하느라 정신이 나갔어요!',
+      '하지만 나름 체계적으로 일하는 편이에요😅',
+      '친구들이랑 놀러가면,<br>혼자 계획을 다 짜요!',
+      '그렇지만 아무도 그 계획을 따르지 않아서 <br>"다신 쟤네랑 안놀아 ㅅㅂ"라고 생각해요!'
+    ],
+    jobs: ['아재셀카 시현<br>얼굴 없어 시현', '사랑니 뽑 시현<br>사와디캅 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '얼굴 없어 시현!!', //11번 istp 완료
+    character: '/시현 이미지/긴머리.jpg',
+    results: [
+      '얼굴이 없어요!',
+      '과묵한 편이라 말도 없어요🫥',
+      '적응력이 강해 눈이 안보여도 불편하지 않아요!',
+      '언제쯤 탈모가 올지 분석해요!'
+    ],
+    jobs: ['양갈래 시현<br>정신나감 시현', '사랑니 뽑 시현<br>사와디캅 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '올빽 시현!!', //12번 intj 완료
+    character: '시현 이미지/올빽.jpg',
+    results: [
+      '자전거 타느라 머리가 다 넘어갔어요! ',
+      '꽤나 독립적이에요😤',
+      '의지가 강해 내 말이 틀린것 같아도 <br>맞다고 우겨요!',
+      '가위바위보 하기 전에 <br>상대한테 뭐 낼건지 물어봐요!'
+    ],
+    jobs: ['드론 시현<br>앞치마소녀 시현', '없음'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '짱구 시현!!', //13번 intp 완료
+    character: '/시현 이미지/짱구.jpg',
+    results: [
+      '불고기 로드 버전 짱구에요!',
+      '호기심이 많아 지나가는 사람의 <br>마스크 속 얼굴을 상상해요🙄',
+      '매일 이슬이 누나랑 결혼할 가능성을 생각해요!',
+      '하지만 현실엔 이슬이 누나가 없죠.'
+    ],
+    jobs: ['핑크아령 시현<br>정신나감 시현', '없음'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '시현의 손발!!', //14번 istj 완료
+    character: '/시현 이미지/손발.jpg',
+    results: [
+      '발가락에 깍지가 껴져요!',
+      '책임감이 강해, 제 할일을 하는 편이에요✋🦶',
+      '매사에 철저해서 발을 헏딛거나 넘어지지 않아요!',
+      '보수적인 편이라 함부러 손모가지를 걸지 않아요!'
+    ],
+    jobs: ['통스팸 시현<br>열올라 시현', '개말라 시현<br>드론 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  },
+  {
+    title: '드론 시현!!', //15번 enfp  완료
+    character:  './시현 이미지/드론머리.jpg',   
+    results: [
+      '머리가 드론이에요~',
+      '언제 어디서든 관심이 필요해요🤩',
+      '똑같은 하루하루에 지루함을 느껴요!',
+      '가끔 하늘을 날고 싶어져요!'
+    ],
+    jobs: ['사랑니 뽑 시현<br>올빽 시현', '아재셀카 시현<br>통스팸 시현'],
+    lectureImg: './시현 이미지/인스타.png',
+    lectureUrl: 'https://www.instagram.com/hyeonsi.__.jeon/'
+  }
+]
+export const mbtis = {
+  entj: 0,
+  entp: 1,
+  estp: 2,
+  esfp: 3,
+  esfj: 4,
+  enfj: 5,
+  infp: 6,
+  isfp: 7,
+  isfj: 8,
+  infj: 9,
+  estj: 10,
+  istp: 11,
+  intj: 12,
+  intp: 13,
+  istj: 14,
+  enfp: 15
+}
